8537e83f3768a1ceab788c88eb0169e5ae1df250
楊　暁 <springam09@gmail.com>
2016-05-04 18:06:19 +0900

    add nickname

--- db/migrate/20160426064617_create_reviews.rb
+++ db/migrate/20160426064617_create_reviews.rb
@@ -2,6 +2,7 @@ class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
       t.text  :review
+      t.string  :nickname
       t.string  :petname
       t.integer  :user_id
       t.timestamps null: false

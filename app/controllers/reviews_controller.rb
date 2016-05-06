fb046433efba8ca4169658af962a3b1a9f18da3d
楊　暁 <springam09@gmail.com>
2016-05-06 12:12:35 +0900

    change review controller 2

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,12 @@
 class ReviewsController < ApplicationController
 
   def create
-    @review = Review.create(diary_id: review_params[:diary_id], user_id: current_user.id)
+    @review = Review.create(text: review_params[:text], diary_id: review_params[:diary_id], user_id: current_user.id)
     redirect_to "/diaries/#{@review.diary.id}"
   end
 
   private
   def review_params
-    params.permit(:diary_id)
+    params.permit(:text, :diary_id)
   end
 end

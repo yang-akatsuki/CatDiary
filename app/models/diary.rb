1cd26b27f2bf0a56ed8f5344662019d7d748fc97
楊　暁 <springam09@gmail.com>
2016-05-12 18:54:34 +0900

    bootstrap適応 twitterの画像取得

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -9,13 +9,29 @@ class Diary < ActiveRecord::Base
       config.access_token = '1313070871-zb8s4F3CnD9FkMIBLS0TzY5zTqeHpFO09Ljscix'
       config.access_token_secret = 'x2JshC7ZbBY1DzZvWJwOzdthZKhfUqTlFRPFzMXUO46Yj'
     end
+
     limit   = 10
     tag = "cat"
     tmp=[]
     client.search("##{tag}", lang: 'ja', result_type: 'recent', count: 1).take(limit).map do |tweet|
-      puts "#{tweet.user.name}: #{tweet.text}"
+      if tweet.media[0].present?
+        {
+          icon: tweet.user.profile_image_url,
+          name: tweet.user.name,
+          text: tweet.text,
+          rt: tweet.retweet_count,
+          media: tweet.media[0].media_url.to_s
+        }
+      else
+        {
+        icon: tweet.user.profile_image_url,
+        name: tweet.user.name,
+        text: tweet.text,
+        rt: tweet.retweet_count
+        }
+      end
       tmp << tweet
     end
     return tmp
- end
+  end
 end
